296cb8802bb79a424e7b339ff07e6f887688a982
Wonil Park <wpark912@gmail.com>
2019-02-23 17:14:36 -0500

    Hello Exercise

--- Hello/MainPage.xaml.cs
+++ Hello/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,12 @@ namespace Hello
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void okClick(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msg = new MessageDialog("Hello " + userName.Text);
+            msg.ShowAsync();
         }
     }
 }
